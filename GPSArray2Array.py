4c1a780c4fb3f6f934c546db5fb67d22a3974b89
sokcioa <sokcioa@gmail.edu>
2025-01-28 18:30:12 -0500

    getting file changes

--- GPSArray2Array.py
+++ GPSArray2Array.py
@@ -32,7 +32,7 @@ def p2arr_dist(point, refarray, rad=0):
   dist_arr = R * c
   return dist_arr
 
-def arr2arr_match(arr1, arr2, rad):
+def arr2arr_match(arr1, arr2, rad=0):
   assert arr1.dtype == float, "arr1 must not contain non-numeric values like None or Strings"
   assert arr2.dtype == float, "arr2 must not contain non-numeric values like None or Strings"
   assert not np.any(np.isnan(arr1)), "arr1 must not contain NaN values."
@@ -69,68 +69,7 @@ def arr2arr_match(arr1, arr2, rad):
   min_dist_idxs = np.argmin(dist_arr, axis=1)
   return min_dist_idxs  
 
-def correct_test():
-  BostonMA = (42.3601, -71.0589)
-  JawsBridgeMA = (41.4164, -70.5482)
-  DorchesterMA = (42.3016, -71.0677)
-  RumneyNH = (43.8073, -71.8159)
-  PlymouthNH = (43.7570, -71.6887)
-  CambridgeMA = (42.3736,- 71.1097)
-  CapeCodMA = (41.6688, -70.2962)
-  ArlingtonMA = (42.4154, -71.1565)
-  QuincyMA = (42.2529, -71.0023)
-  BangorME = (44.8012, -68.7778)
-  arr1 = np.array([BostonMA, RumneyNH, CapeCodMA])
-  arr2 = np.array([DorchesterMA,CambridgeMA,ArlingtonMA,BangorME,PlymouthNH,QuincyMA, JawsBridgeMA])
-  answer = np.array([0, 0, 0, 1, 1, 0, 2], 'int')
-  print(f"{arr2arr_match(arr2, arr1, 0)}\n{answer}")
 
-def fail_test1(): 
-  BostonMA = (42.3601, -71.0589)
-  JawsBridgeMA = (41.4164, -270.5482)
-  DorchesterMA = (42.3016, -71.0677)
-  RumneyNH = (43.8073, -71.8159)
-  PlymouthNH = (43.7570, -71.6887)
-  CambridgeMA = (42.3736,- 71.1097)
-  CapeCodMA = (41.6688, -70.2962)
-  ArlingtonMA = (42.4154, -71.1565)
-  QuincyMA = (42.2529, -71.0023)
-  BangorME = (44.8012, -68.7778)
-  arr1 = np.array([BostonMA, RumneyNH, CapeCodMA])
-  arr2 = np.array([DorchesterMA,CambridgeMA,ArlingtonMA,BangorME,PlymouthNH,QuincyMA, JawsBridgeMA])
-  arr2arr_match(arr1, arr2, 0)
-
-def fail_test2(): 
-  BostonMA = (42.3601, -71.0589)
-  JawsBridgeMA = (41.4164, None)
-  DorchesterMA = (42.3016, -71.0677)
-  RumneyNH = (43.8073, -71.8159)
-  PlymouthNH = (43.7570, -71.6887)
-  CambridgeMA = (42.3736,- 71.1097)
-  CapeCodMA = (41.6688, -70.2962)
-  ArlingtonMA = (42.4154, -71.1565)
-  QuincyMA = (42.2529, -71.0023)
-  BangorME = (44.8012, -68.7778)
-  arr1 = np.array([BostonMA, RumneyNH, CapeCodMA])
-  arr2 = np.array([DorchesterMA,CambridgeMA,ArlingtonMA,BangorME,PlymouthNH,QuincyMA, JawsBridgeMA])
-  arr2arr_match(arr1, arr2, 0)
-
-def fail_test3(): 
-  BostonMA = ("42.3601", "-71.0589")
-  JawsBridgeMA = (41.4164, -70.5482)
-  DorchesterMA = (42.3016, -71.0677)
-  RumneyNH = ("43.8073", "-71.8159")
-  PlymouthNH = (43.7570, -71.6887)
-  CambridgeMA = (42.3736,- 71.1097)
-  CapeCodMA = ("41.6688", "-70.2962")
-  ArlingtonMA = (42.4154, -71.1565)
-  QuincyMA = (42.2529, -71.0023)
-  BangorME = (44.8012, -68.7778)
-  arr1 = np.array([BostonMA, RumneyNH, CapeCodMA])
-  arr2 = np.array([DorchesterMA,CambridgeMA,ArlingtonMA,BangorME,PlymouthNH,QuincyMA, JawsBridgeMA])
-  arr2arr_match(arr1, arr2, 0)
-
-def test():
   correct_test()
   try:
     fail_test1()
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from GPSArray2Array import *
 from ImportFromCSV import *
-RA_FP = ""  #Reference Array
-TA_FP = ""  #Target Array
+BP = "/Users/thomas/Documents/EC530"
+RA_FP = "Major_Cities_GPS.csv"  #Reference Array
+TA_FP = "iata-icao.csv"  #Target Array
+RA = import_coordinates_from_csv(BP+'/'+ RA_FP)
+TA = import_coordinates_from_csv(BP+'/'+ TA_FP)
+MA = arr2arr_match(RA,TA)
+
+#for i in range(10):
+#    print(RA[i])
+#    idx = MA[i]
+#    print(TA[idx])
+
